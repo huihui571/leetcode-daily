68f33f64505d9e2d895e53ed5b32c4230bf03e74
huihui571 <weina_h@foxmail.com>
2020-12-13 15:30:39 +0800

    添加对号入座法

--- 第3章 高质量的代码/3.3 代码的完整性/21-调整数组顺序使奇数位于偶数前面.cpp
+++ 第3章 高质量的代码/3.3 代码的完整性/21-调整数组顺序使奇数位于偶数前面.cpp
@@ -20,9 +20,11 @@
 
 using namespace std;
 
+#if 1
 /**
  * 1. 双指针法，时间复杂度O(n)，空间复杂度O(1)
  * 2. 辅助数组法，时间复杂度O(n)，空间复杂度O(n)
+ * 3. 快慢指针法，对号入座，时间O(n)，空间O(1)
  */
 class Solution {
 public:
@@ -55,3 +57,41 @@ public:
         nums[j] = t;
     }
 };
+#endif
+
+#if 0
+/**
+ * 对号入座法，link:jz-03-数组中的重复数字,283-移动零
+ * 奇数元素的座位应该在数组左半部分从0开始递增，用一个指针记录奇数元素的个数，即它们的“座位”，然后把每个奇数元素放到它的座位上去。
+ * 由于采用swap这种方法会破坏偶数元素的相对位置
+ */
+class Solution {
+public:
+    vector<int> exchange(vector<int>& nums) {
+        if (nums.empty())
+            return {};
+
+        int j = 0;  //记录下一个奇数元素应该在的位置，即“座位”
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!isEven(nums[i])) {
+                if (i != j)                //避免自己和自己交换
+                    swap(nums, i, j);   //把奇数元素i放到它的座位去
+                j++;                       //遇到奇数元素j+1
+            }
+        }
+
+        return nums;
+    }
+
+    //判断val是否为偶数，采用位运算
+    bool isEven(int val) {
+        return (val & 0x01) == 0;
+    }
+
+    void swap(vector<int>& nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+};
+#endif
