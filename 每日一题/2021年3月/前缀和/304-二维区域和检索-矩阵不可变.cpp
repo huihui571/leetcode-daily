52785e781d70762993de2833ebc9b6bb505163a4
huihui571 <34211367+huihui571@users.noreply.github.com>
2021-03-04 20:52:37 +0800

    前缀和两个小题

---
+++ 每日一题/2021年3月/前缀和/304-二维区域和检索-矩阵不可变.cpp
@@ -0,0 +1,41 @@
+/*<FH+>************************************************************************
+*   Editor      : Vim
+*   File name   : 304-二维区域和检索-矩阵不可变.cpp 
+*   Author      : huihui571
+*   Created date: 2021-03-03
+*   Description : 给定一个二维矩阵，计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1, col1) ，右下角为 (row2, col2) 。
+*
+*<FH->************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * 二维前缀和
+ */
+class NumMatrix {
+private:
+    vector<vector<int>> sums;
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        if (m > 0) {
+            int n = matrix[0].size();
+            sums.resize(m + 1, vector<int>(n + 1));
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    sums[i + 1][j + 1] = sums[i][j + 1] + sums[i + 1][j] - sums[i][j] + matrix[i][j];
+                }
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1]; 
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
